c373dca56795461d73a1b6cab2ef3ec218e8ab5d
Antoine Cheminat <ac.dev@hotmail.com>
2023-09-23 08:38:43 -0400

    Setup postcss-nested

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcssNested from 'postcss-nested';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [postcssNested()],
+    },
+  },
+});
