4a855ccb2af2660f8ab4cb90a7044c6552afc0cf
Antoine Cheminat <ac.dev@hotmail.com>
2023-09-25 05:50:28 -0400

    Create tablet mediaqueries

--- src/components/TabNavigation/TabNavigation.tsx
+++ src/components/TabNavigation/TabNavigation.tsx
@@ -11,17 +11,13 @@ interface TabNavigationProps {
 
 function TabNavigation(props: TabNavigationProps) {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} style={{ '--accent-color': props.accentClr } as React.CSSProperties}>
       {TAB_ITEMS.map((item, index) => (
-        <div className={styles.tab}>
+        <div className={styles.tab} data-active={props.activeTab === item}>
           <button key={index} onClick={() => props.action(item)}>
             {item}
           </button>
-          <div
-            aria-hidden="true"
-            data-visible={props.activeTab === item}
-            style={{ backgroundColor: `hsl(${props.accentClr})` }}
-          />
+          <div aria-hidden="true" />
         </div>
       ))}
     </div>
